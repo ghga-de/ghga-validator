db898f0f75239e2e698ac5535d104ba1de26553b
Galina Tremper <g.tremper@dkfz-heidelberg.de>
2023-10-10 09:42:34 +0000

    Fix small issue in tests

--- tests/test_validate_json_schema.py
+++ tests/test_validate_json_schema.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Test structural validation using JSON schema"""
+
 import os
 
 from ghga_validator.cli import validate_json
